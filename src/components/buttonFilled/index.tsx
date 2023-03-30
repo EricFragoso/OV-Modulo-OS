d2b02a8099f5673b2fbe4443f72be56ef1f0cf81
caiovfreitas <cvaf@cin.ufpe.br>
2023-03-30 11:54:29 -0300

    Ajustes Componente Botão

--- src/components/buttonFilled/index.tsx
+++ src/components/buttonFilled/index.tsx
@@ -4,11 +4,10 @@ type Props = {
 	text: string;
 	fontSize?: number;
 	borderRadius?: number;
-	marginBottom?: number;
 	callFunc: () => void;
 };
 
-export function Button(props: Props) {
+export function ButtonFilled(props: Props) {
 	return (
 		<View>
 			<TouchableOpacity
@@ -17,17 +16,14 @@ export function Button(props: Props) {
 					justifyContent: "center",
 					paddingHorizontal: 28,
 					paddingVertical: 14,
-					backgroundColor: "#FFF",
-					borderWidth: 1.5,
-					borderColor: "#459EE8",
+					backgroundColor: "#2B3049",
 					borderRadius: props.borderRadius || 10,
-					marginBottom: props.marginBottom || 12,
 				}}
 				onPress={props.callFunc}
 			>
 				<Text
 					style={{
-						color: "#2B3049",
+						color: "#DAECFA",
 						fontSize: props.fontSize || 24,
 						fontWeight: "bold",
 					}}
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,55 +1,45 @@
-import React, { useState } from 'react'
-import { Text, View, TextInput, Alert  } from 'react-native'
-import { styles } from './styles'
+import React, { useState } from "react";
+import { Text, View, TextInput, Alert } from "react-native";
+import { styles } from "./styles";
 
-import { Button } from '../../components/button'
- 
+import { Button } from "../../components/button";
+
 export function Home() {
+	const [userCode, setUserCode] = useState("");
 
-  const [userCode, setUserCode] = useState('')
+	function handleUserLogin(codigo: string) {
+		if (codigo == "") {
+			return Alert.alert("Atenção", "Código em branco");
+		} else if (codigo == "00") {
+			return Alert.alert("Atenção", "Código inválido");
+		}
 
-  function handleUserLogin(codigo: string) {
-    if(codigo == ''){
-      return Alert.alert("Atenção", "Código em branco")
-    } else if(codigo == '00'){
-      return Alert.alert("Atenção", "Código inválido")
-    }
+		console.log(codigo);
+	}
 
-    console.log(codigo);
-    
-  }
+	return (
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<Text style={styles.headerText}>Overview</Text>
+			</View>
 
- return(
-  
-  <View 
-  style={styles.container}>
-    
-    <View style={styles.header}>
+			<View style={styles.input}>
+				<Text style={styles.inputText}>Código do usuário</Text>
 
-    <Text 
-    style={styles.headerText}>
-      Overview
-    </Text>
-      </View>
-    
-    <View style={styles.input}>
-    <Text 
-    style={styles.inputText}>
-      Código do usuário
-    </Text>
-    
-    <TextInput 
-    style={styles.inputField}
-    keyboardType='numeric'
-    placeholder='Insira o código'
-    placeholderTextColor={'#000'}
-    onChangeText={inputText => setUserCode(inputText)}
-    />
-    </View>
+				<TextInput
+					style={styles.inputField}
+					keyboardType="numeric"
+					placeholder="Insira o código"
+					placeholderTextColor={"#000"}
+					onChangeText={(inputText) => setUserCode(inputText)}
+				/>
+			</View>
 
-    <Button text='Enviar' callFunc={() => handleUserLogin(userCode)} />
-
-  </View>
- )
+			<Button
+				text="Enviar"
+				fontSize={24}
+				callFunc={() => handleUserLogin(userCode)}
+			/>
+		</View>
+	);
 }
-
