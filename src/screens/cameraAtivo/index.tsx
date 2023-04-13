33340c0f93266fd5c112940fac740af11a1a5f6f
caiovfreitas <cvaf@cin.ufpe.br>
2023-04-13 10:06:59 -0300

    Layout da tela de câmera

--- src/screens/cameraAtivo/index.tsx
+++ src/screens/cameraAtivo/index.tsx
@@ -1,7 +1,17 @@
 import { Camera, CameraType } from "expo-camera";
 import React, { useState, useEffect, useRef } from "react";
-import { Text, View, Modal, Image } from "react-native";
+import {
+	Text,
+	View,
+	Modal,
+	Image,
+	ImageBackground,
+	TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import { Button } from "../../components/button";
+import { MenuHamburger } from "../../components/menuHamburger";
+import { BackButton } from "../../components/backButton";
 
 export function CameraAtivo() {
 	const [type, setType] = useState(CameraType.back);
@@ -9,6 +19,7 @@ export function CameraAtivo() {
 	const [photoTaken, setPhotoTaken] = useState(null);
 	const [open, setOpen] = useState(false);
 	const camRef = useRef(null);
+	const navigation = useNavigation();
 
 	useEffect(() => {
 		(async () => {
@@ -30,19 +41,46 @@ export function CameraAtivo() {
 		}
 	}
 
+	function handleCallPreviousPage() {
+		navigation.goBack();
+	}
+
 	return (
-		<View className="flex-1 justify-center">
-			<Camera
-				className="flex-1 items-center justify-end p-10"
-				type={type}
-				ref={camRef}
+		<View className="flex-1">
+			<ImageBackground
+				className="flex-1 w-full"
+				source={require("../../assets/img/Overview-50-BG.png")}
 			>
-				<Button
-					text="Tirar Foto"
-					marginBottom={0}
-					callFunc={handleTakePhoto}
-				></Button>
-			</Camera>
+				<View className="w-full h-32 flex-row justify-between items-center bg-[#459EE8] pt-14 px-8">
+					<Image
+						className={"w-11 h-11"}
+						source={require("../../assets/img/CG-Preto.png")}
+					/>
+					<Text className="flex-1 text-center font-OpenSansBold text-3xl">
+						Foto do Ativo
+					</Text>
+					<MenuHamburger />
+				</View>
+				<View className="flex-row items-center my-2 ml-3">
+					<BackButton callFunc={handleCallPreviousPage} />
+
+					<TouchableOpacity onPress={handleCallPreviousPage}>
+						<Text className="font-OpenSansBold text-xl">Voltar</Text>
+					</TouchableOpacity>
+				</View>
+				<View className="px-5">
+					<Camera
+						className="h-4/6 mb-5"
+						type={type}
+						ref={camRef}
+					></Camera>
+					<Button
+						text="Tirar Foto"
+						marginBottom={0}
+						callFunc={handleTakePhoto}
+					/>
+				</View>
+			</ImageBackground>
 
 			{photoTaken && (
 				<Modal
